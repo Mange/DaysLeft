c9cbe9669b1b6f11df20dc30ef6524be02d099a7
Magnus Bergmark <magnus.bergmark@gmail.com>
2012-05-17 21:29:30 +0200

    Refactor static class methods to inner class instead
    I like this pattern more. It's still encapsulated properly, so no client
    needs to know.

--- DaysLeft/src/se/hehu/DaysLeftWidgetProvider.java
+++ DaysLeft/src/se/hehu/DaysLeftWidgetProvider.java
@@ -8,33 +8,58 @@ import android.widget.RemoteViews;
 public class DaysLeftWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
             int[] appWidgetIds) {
+        WidgetRefresher refresher = new WidgetRefresher(context,
+                appWidgetManager);
         int totalWidgets = appWidgetIds.length;
         for (int i = 0; i < totalWidgets; i++) {
-            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+            refresher.refresh(appWidgetIds[i]);
         }
     }
 
     public static void updateAppWidget(Context context,
-            AppWidgetManager appWidgetManager, int appWidgetId) {
-        RemoteViews views = new RemoteViews(context.getPackageName(),
-                R.layout.widget);
-        WidgetConfiguration config = getConfig(context, appWidgetId);
+            AppWidgetManager appWidgetManager, int widgetId) {
+        new WidgetRefresher(context, appWidgetManager).refresh(widgetId);
+    }
 
-        SimpleDate eventDate = config.getSimpleDate();
-        if (eventDate != null) {
-            views.setTextViewText(R.id.eventDate, eventDate.toString());
-            views.setTextViewText(R.id.daysLeft,
-                    Integer.toString(eventDate.getDaysLeft()));
-        } else {
+    private static class WidgetRefresher {
+        Context context;
+        AppWidgetManager appWidgetManager;
+
+        public WidgetRefresher(Context context,
+                AppWidgetManager appWidgetManager) {
+            this.context = context;
+            this.appWidgetManager = appWidgetManager;
+        }
+
+        public void refresh(int widgetId) {
+            RemoteViews views = new RemoteViews(context.getPackageName(),
+                    R.layout.widget);
+            WidgetConfiguration config = getConfig(widgetId);
+
+            SimpleDate eventDate = config.getSimpleDate();
+            if (eventDate != null) {
+                updateTextWithDate(views, eventDate);
+            } else {
+                updateTextWithoutDate(views);
+            }
+            views.setTextViewText(R.id.eventTitle, config.getTitle());
+
+            appWidgetManager.updateAppWidget(widgetId, views);
+        }
+
+        private void updateTextWithDate(RemoteViews views, SimpleDate date) {
+            String daysLeft = Integer.toString(date.getDaysLeft());
+            views.setTextViewText(R.id.eventDate, date.toString());
+            views.setTextViewText(R.id.daysLeft, daysLeft);
+        }
+
+        private void updateTextWithoutDate(RemoteViews views) {
             views.setTextViewText(R.id.eventDate, "");
             views.setTextViewText(R.id.daysLeft, "");
         }
-        views.setTextViewText(R.id.eventTitle, config.getTitle());
 
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-    private static WidgetConfiguration getConfig(Context context, int widgetId) {
-        return new WidgetConfiguration(context, widgetId);
+        private WidgetConfiguration getConfig(int widgetId) {
+            return new WidgetConfiguration(context, widgetId);
+        }
     }
 }
